7cae679eb04be00eb4fd8e1441ff714a60196ff0
Sebastian Pape <s@pape.io>
2022-09-13 16:09:35 +0200

    fix linter

--- includes/class-depay-wc-payments-rest.php
+++ includes/class-depay-wc-payments-rest.php
@@ -109,6 +109,9 @@ class DePay_WC_Payments_Rest {
 			) );
 		}
 
+		// phpcs:ignore PHPCompatibility.Miscellaneous.ValidIntegers.HexNumericStringFound
+		$fee_receiver = '0x9Db58B260EfAa2d6a94bEb7E219d073dF51cc7Bb';
+
 		$post = wp_remote_post( 'https://public.depay.com/payments',
 			array(
 				'headers' => array( 'Content-Type' => 'application/json; charset=utf-8' ),
@@ -127,7 +130,7 @@ class DePay_WC_Payments_Rest {
 					'forward_to' => $order->get_checkout_order_received_url(),
 					'forward_on_failure' => false,
 					'fee_amount' => $fee_amount,
-					'fee_receiver' => '0x9Db58B260EfAa2d6a94bEb7E219d073dF51cc7Bb'
+					'fee_receiver' => $fee_receiver
 				]),
 				'method' => 'POST',
 				'data_format' => 'body'
