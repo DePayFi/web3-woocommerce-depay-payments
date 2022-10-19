6b8d9c6378b64e5074ee3e5f5462d637ebcdc155
Sebastian Pape <s@pape.io>
2022-10-19 20:38:52 +0200

    add changelog.txt

--- update-version.js
+++ update-version.js
@@ -14,12 +14,12 @@ prompt.get(['version'], async (err, result)=> {
   
   await replace({ files: './package.json', from: `"version": "${package.version}",`, to: `"version": "${result.version}",`})
   await replace({ files: './readme.txt', from: `Stable tag: ${package.version}`, to: `Stable tag: ${result.version}`})
-  await replace({ files: './readme.txt', from: `== Changelog ==`, to: `== Changelog ==\n\n= ${result.version} =\n*`})
+  await replace({ files: './changelog.txt', from: `= DePay Web3 Payments for WooCommerce =`, to: `= DePay Web3 Payments for WooCommerce =\n\n== ${result.version} ==\n*`})
   await replace({ files: './depay-woocommerce-payments.php', from: `* Version: ${package.version}`, to: `* Version: ${result.version}`})
   await replace({ files: './depay-woocommerce-payments.php', from: `define( 'DEPAY_CURRENT_VERSION', '${package.version}' );`, to: `define( 'DEPAY_CURRENT_VERSION', '${result.version}' );`})
   await replace({ files: './languages/depay-woocommerce-payments.pot', from: `Project-Id-Version: DePay WooCommerce Payments ${package.version}`, to: `Project-Id-Version: DePay WooCommerce Payments ${result.version}`})
 
-  console.log('Dont forget to add a changelog entry to readme.txt')
-  openExplorer('./readme.txt')
+  console.log('Dont forget to add a changelog entry!')
+  openExplorer('./changelog.txt')
 })
 
