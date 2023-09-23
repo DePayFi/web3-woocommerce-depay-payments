f8c00815f916a4054e70049ea3f0ce85a25598b8
Sebastian Pape <s@pape.io>
2023-09-23 12:27:30 +0200

    v2.1.2: fixes checkout button (visually)

--- includes/class-depay-wc-payments-gateway.php
+++ includes/class-depay-wc-payments-gateway.php
@@ -25,7 +25,7 @@ class DePay_WC_Payments_Gateway extends WC_Payment_Gateway {
 	}
 
 	public function get_icon() {
-		$icon = '<style>.payment_method_depay_wc_payments { display: flex; flex-direction: column; } .payment_method_depay_wc_payments label { display: flex !important; flex-grow: 1; align-items: center; }</style><div style="display: inline; flex-grow: 0;">';
+		$icon = '<style>.payment_box.payment_method_depay_wc_payments { width: 100%; } .wc_payment_method.payment_method_depay_wc_payments { display: flex; flex-direction: row; flex-wrap: wrap; } .payment_method_depay_wc_payments label { display: flex !important; flex-grow: 1; align-items: center; }</style><div style="display: inline; flex-grow: 0;">';
 		if ( empty( get_option( 'depay_wc_blockchains' ) ) ) {
 			return $icon;
 		}
