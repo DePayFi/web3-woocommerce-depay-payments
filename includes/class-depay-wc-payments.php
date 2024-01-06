820e933893a83da944ade9a0ba553db4a7ab8f9e
Sebastian Pape <0xneo11@gmail.com>
2024-01-06 10:38:17 +0100

    v2.7.0: upgrade widgets & load frontend javascript defer to improve page speed

--- includes/class-depay-wc-payments.php
+++ includes/class-depay-wc-payments.php
@@ -123,9 +123,9 @@ class DePay_WC_Payments {
 	
 	public static function setup_checkout_scripts() {
 
-		wp_register_script( 'DEPAY_WC_WIDGETS', plugins_url( 'dist/widgets.bundle.js', DEPAY_WC_PLUGIN_FILE ), array(), DEPAY_CURRENT_VERSION, true );
+		wp_register_script( 'DEPAY_WC_WIDGETS', plugins_url( 'dist/widgets.bundle.js', DEPAY_WC_PLUGIN_FILE ), array(), DEPAY_CURRENT_VERSION, array( 'in_footer' => true, 'strategy' => 'defer' ) );
 		wp_enqueue_script( 'DEPAY_WC_WIDGETS' );
-		wp_register_script( 'DEPAY_WC_CHECKOUT', plugins_url( 'dist/checkout.js', DEPAY_WC_PLUGIN_FILE ), array( 'wp-api-request', 'jquery' ), DEPAY_CURRENT_VERSION, true );
+		wp_register_script( 'DEPAY_WC_CHECKOUT', plugins_url( 'dist/checkout.js', DEPAY_WC_PLUGIN_FILE ), array( 'wp-api-request', 'jquery' ), DEPAY_CURRENT_VERSION, array( 'in_footer' => true, 'strategy' => 'defer' ) );
 		wp_enqueue_script( 'DEPAY_WC_CHECKOUT' );
 		wp_localize_script('DEPAY_WC_CHECKOUT', 'DEPAY_WC_CURRENCY', array(
 			'displayCurrency' => ( get_option('depay_wc_displayed_currency') ),
