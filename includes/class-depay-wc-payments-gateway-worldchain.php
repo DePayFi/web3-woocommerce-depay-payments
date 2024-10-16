d72a93d98b00341c6822468aea465275ec22314d
Sebastian Pape <0xneo11@gmail.com>
2024-10-16 19:51:46 +0200

    convert tabs to spaces

--- includes/class-depay-wc-payments-gateway-worldchain.php
+++ includes/class-depay-wc-payments-gateway-worldchain.php
@@ -1,23 +1,23 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) {
-  exit;
+	exit;
 }
 
 class DePay_WC_Payments_Worldchain_Gateway extends DePay_WC_Payments_Gateway {
 
-  const GATEWAY_ID = 'depay_wc_payments_worldchain';
+	const GATEWAY_ID = 'depay_wc_payments_worldchain';
 
-  public function __construct() {
-    $this->id                 = static::GATEWAY_ID;
-    $this->method_title       = 'World Chain';
-    $this->method_description = 'Payments on World Chain.';
-    $this->supports           = [ 'products' ];
-    $this->init_form_fields();
-    $this->init_settings();
-    $title                    = 'World Chain';
-    $this->title              = $title;
-    $description              = get_option( 'depay_wc_checkout_description_worldchain' );
-    $this->description        = empty($description) ? null : $description;
-    $this->blockchain         = 'worldchain';
-  }
+	public function __construct() {
+		$this->id                 = static::GATEWAY_ID;
+		$this->method_title       = 'World Chain';
+		$this->method_description = 'Payments on World Chain.';
+		$this->supports           = [ 'products' ];
+		$this->init_form_fields();
+		$this->init_settings();
+		$title                    = 'World Chain';
+		$this->title              = $title;
+		$description              = get_option( 'depay_wc_checkout_description_worldchain' );
+		$this->description        = empty($description) ? null : $description;
+		$this->blockchain         = 'worldchain';
+	}
 }
